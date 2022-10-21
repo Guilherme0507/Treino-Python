eefe806d27edc2670751445a952d1a2ed9e205ba
Guilherme0507 <guilherme_alves.contato@outlook.com>
2022-10-21 15:53:03 -0300

    Avancando no treinamento de POO utilizando Python

---
+++ POO-Basico/dataclasse.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, lastname):
+        self.name = name
+        self.lastname = lastname
+
+        def __repr__(self)
+
+if __name__ == '__main__':
+    john = Person('John', 'Doe')
+    print(john)
